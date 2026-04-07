95092bc0fd4001401d85b1c0df356c0e50bfa95f
nekosu <liao.hy@outlook.com>
2026-04-08 01:17:24 +0800

    feat: support string
    link compiler-rt

--- src/lib/libcxx.cpp
+++ src/lib/libcxx.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <__verbose_abort>
+#include <new>
+#include <string>
 
 #include "../arch/io.hpp"
 
@@ -26,10 +28,26 @@ void* operator new[](size_t size) {
     return malloc(size);
 }
 
+void* operator new(size_t size, std::align_val_t) {
+    // TODO: check align
+    return malloc(size);
+}
+
 void operator delete(void* ptr) noexcept {
+    return free(ptr);
+}
+
+void operator delete(void* ptr, size_t) noexcept {
+    return free(ptr);
+}
+
+void operator delete(void* ptr, size_t, std::align_val_t) noexcept {
+    // TODO: check align
     return free(ptr);
 }
 
 void operator delete[](void* ptr) noexcept {
     return free(ptr);
 }
+
+template class std::basic_string<char>;
