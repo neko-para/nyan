0e69853460800e21954fbe6248d4440bd9fb2f5a
nekosu <liao.hy@outlook.com>
2026-04-15 21:23:47 +0800

    refactor: split

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,10 @@
 #include "allocator/load.hpp"
 #include "boot/entry.hpp"
 #include "data/embed.hpp"
-#include "elf/entry.hpp"
 #include "gdt/load.hpp"
 #include "interrupt/load.hpp"
 #include "keyboard/load.hpp"
-#include "keyboard/message.hpp"
-#include "lib/containers.hpp"
 #include "lib/format.hpp"
-#include "lib/function.hpp"
 #include "paging/kernel.hpp"
 #include "syscall/entry.hpp"
 #include "task/task.hpp"
@@ -23,6 +19,16 @@ extern "C" void __libc_init_array();
 extern uint8_t _end;
 
 namespace nyan {
+
+int logic(void*) {
+    vga::print("kernel end {#010x}\n", paging::VirtualAddress(&_end).kernelToPhysical().addr);
+
+    auto tcb = task::createElfTask(&_binary_prog_bin_start[0], &_binary_prog_bin_end[0] - &_binary_prog_bin_start[0]);
+    auto pid = task::addTask(tcb);
+
+    syscall::waitpid(pid, 0, 0);
+    return 0;
+}
 
 extern "C" void kmain(boot::BootInfo* info) {
     arch::enableSse();
@@ -57,30 +63,12 @@ extern "C" void kmain(boot::BootInfo* info) {
         break;
     }
 
-    arch::sti();
-
-    vga::print("kernel end {#010x}\n", paging::VirtualAddress(&_end).kernelToPhysical().addr);
-
     task::load();
-
-    pid_t pid;
-    {
-        auto tcb =
-            task::createElfTask(&_binary_prog_bin_start[0], &_binary_prog_bin_end[0] - &_binary_prog_bin_start[0]);
-        pid = task::addTask(tcb);
-    }
-
-    task::addTask(task::createTask(
-        +[](void* param) {
-            syscall::waitpid(reinterpret_cast<pid_t>(param), 0, 0);
-            return 0;
-        },
-        reinterpret_cast<void*>(pid)));
-
+    task::addTask(task::createTask(logic));
     task::initYield();
 
+    arch::sti();
     vga::print("all tasks finished.\n");
-
     for (int i = 0; i < task::MaxTaskCount; i++) {
         if (task::allTasks[i]) {
             task::allTasks[i]->dump();
