74daeaba93d8d1c7d202c0a5a4ce2b925c6fe6c4
nekosu <liao.hy@outlook.com>
2026-04-26 23:50:49 +0800

    chore: update scripts

--- scripts/build-musl-full.sh
+++ scripts/build-musl-full.sh
@@ -8,3 +8,15 @@ export LIBCC=/opt/llvm-22/lib/clang/22/lib/generic/libclang_rt.builtins-i386.a
 mkdir -p build-musl-full
 cd build-musl-full
 ../nyan-env/musl/configure --prefix="$SYSROOT/usr" --disable-shared
+make -j 16
+make install -j 16
+cd ..
+
+export CFLAGS="-g -Og"
+mkdir -p build-musl-full-debug
+cd build-musl-full-debug
+../nyan-env/musl/configure --prefix="$SYSROOT/usr" --disable-shared --libdir="$SYSROOT/usr/lib/debug"
+make -j 16
+make install -j 16
+cd ..
+
