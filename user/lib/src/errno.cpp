af18b7e090db21c98848cefd24ef8d9604ce4284
nekosu <liao.hy@outlook.com>
2026-04-27 13:15:44 +0800

    chore: remove old libc

--- user/lib/src/errno.cpp
+++
@@ -1,7 +0,0 @@
-#include <errno.h>
-
-int __errno;
-
-extern "C" int* __errno_location() {
-    return &__errno;
-}
--- user/lib/src/libc.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdio_impl.h"
-
-extern "C" void __init_libc() {
-    __init_stdio();
-}
-
-extern "C" void __fini_libc() {
-    __fini_stdio();
-}
